ff5e5565ecd849b0abd96ed8f09df0cd48a33532
Prathvi Puthran <prathvi.puthran@planview.com>
2024-09-09 23:25:40 +0530

    Wishlist added

---
+++ CaseStudyWishlist/Models/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaseStudyWishlist.Models;
+
+public partial class Inventory
+{
+    public int InventoryId { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public DateTime? RestockDate { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
---
+++ CaseStudyWishlist/Models/Ordertable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaseStudyWishlist.Models;
+
+public partial class Ordertable
+{
+    public int OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual Payment? Payment { get; set; }
+
+    public virtual Shipment? Shipment { get; set; }
+
+    public virtual Usertable? User { get; set; }
+}
